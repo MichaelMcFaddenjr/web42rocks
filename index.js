1e8a5d697f24fa321007bc14b9cd26e9643edb75
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-06-03 11:21:01 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hey')
+require('dotnev').config()
+const express = require('express')
+const server = express()
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('this mean this code is deployed');
+}
+
+const PORT = process.env.PORT || 5000
+console.log('port is ->', PORT);
+
+server.get('/api', (req, res) => {
+  res.json({
+    message: `${process.env.cohort} Rocks`
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`listenting on ${PORT}`);
+})
+
